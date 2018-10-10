f9d22dbea5412b6271e4959d0fbab6a0258519f5
Fedor Kovalev <fedor.kovalev@booking.com>
2018-10-10 16:39:50 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'requirements.txt')) as f:
 
 setup(
     name='subtokenizer',
-    version='0.0.2',
+    version='0.0.3',
 
     description='Booking python library for machine translation',
     long_description=long_description,
